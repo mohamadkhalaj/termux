8e8eb9e5982b68bb0e9b4ae67930f54e1c4b9b41
mohammad <mohammadkhalaj@outlook.com>
2020-08-09 16:04:17 +0430

    fixed

--- termux/termux/settings.py
+++ termux/termux/settings.py
@@ -119,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, "static/")
+STATIC_URL = "/static/"
+STATIC_ROOT = "static/"
