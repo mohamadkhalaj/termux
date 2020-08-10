43dc3e371c73e18d72f7d08eb81d7278c56ffa75
mohammad <mohammadkhalaj@outlook.com>
2020-08-10 15:44:11 +0430

    better

--- termux/web/utils.py
+++ termux/web/utils.py
@@ -4,6 +4,5 @@ def toDateTimeobj(date):
     date, time = str(date).split(" ")
     year, month, day = date.split('-')
     hour, min = time.split(":")
-
-    return datetime(int(year), int(month.lstrip("0")), int(day.lstrip("0")), int(hour.lstrip("0")), int(min.lstrip("0")))
-
+    dateobj = datetime(int(year), int(month.lstrip("0")), int(day.lstrip("0")), int(hour.lstrip("0")), int(min.lstrip("0")))
+    return dateobj
