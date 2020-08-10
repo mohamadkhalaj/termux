d1ef86b654cb1a7a2e3ead2aa6ddec87b42d5076
mohammad <mohammadkhalaj@outlook.com>
2020-08-10 16:26:31 +0430

    better

--- termux/web/utils.py
+++ termux/web/utils.py
@@ -7,14 +7,20 @@ def toDateTimeobj(date):
     time_splitted = time.split(":")
     if len(time_splitted) == 3:
         hour, min, sec = time.split(":")
-        if hour == '24':
+        if hour == "24":
             hour = "00"
+        if min == "60":
+            min = "00"
+        if sec == "60":
+            sec = "00"
         dateobj = datetime(normalizer(year), normalizer(month.lstrip("0")), normalizer(day.lstrip("0")), normalizer(hour.lstrip("0")),
                            normalizer(min.lstrip("0")), normalizer(sec.lstrip("0")))
     else:
         hour, min = time.split(":")
-        if hour == '24':
+        if hour == "24":
             hour = "00"
+        if min == "60":
+            min = "00"
         dateobj = datetime(normalizer(year), normalizer(month.lstrip("0")), normalizer(day.lstrip("0")), normalizer(hour.lstrip("0")),
                            normalizer(min.lstrip("0")))
     return dateobj
@@ -27,3 +33,4 @@ def normalizer(string):
     else:
         string = int(string)
     return string
+
